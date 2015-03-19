077aeec701645efb89fe18f13f489e85a817c1c7
Connor Smith <cjs.smith1993@gmailcom>
2015-03-19 01:11:50 -0600

    added Touch library

---
+++ libraries/TouchShield/digits.h
@@ -0,0 +1,18 @@
+// 11 max digits used
+#define DIGITS 11
+
+// Match key inputs with electrode numbers
+#define ONE 8
+#define TWO 5
+#define THREE 2
+#define FOUR 7
+#define FIVE 4
+#define SIX 1
+#define SEVEN 6
+#define EIGHT 3
+#define NINE 0
+
+//extras (not used)
+#define ELE9 9
+#define ELE10 10
+#define ELE11 11
