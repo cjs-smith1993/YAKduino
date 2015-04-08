1d3ea10fa6378920e47545b1da185f08a1ff0ccc
Connor Smith <cjs.smith1993@gmailcom>
2015-04-07 20:42:19 -0600

    moved definitions back into lab7defs

--- Lab7/lab7.ino
+++ Lab7/lab7.ino
@@ -11,16 +11,9 @@ Description: Application code for EE 425 lab 7 (Event flags)
 #include <mpr121.h>
 #include <i2c.h>
 #include <digits.h>
+#include <lab7defs.ino>
 
 extern LCD lcd;
-
-#define EVENT_A_KEY		0x1
-#define EVENT_B_KEY		0x2
-#define EVENT_C_KEY		0x4
-
-#define EVENT_1_KEY		0x1
-#define EVENT_2_KEY		0x2
-#define EVENT_3_KEY		0x4
 
 #define TASK_STACK_SIZE	512				/* stack size in words */
 
